18bd1be886fde6e3e0d952829c478a59bbae8679
Steve Cockram <stevejohncockram@gmail.com>
2011-08-23 14:46:24 +0100

    Ready for Processing

---
+++ Mutator.m
@@ -0,0 +1,225 @@
+classdef Mutator
+  %MUTATOR Only changes the interaction matrix.
+
+  properties
+    %% Unique Mutation Settings
+    % 1 - absolute
+    % 0 - relative
+    perturb_style = 1;
+    
+    % Clear
+    perturb_severity = 0.1
+    
+    % 1 - perturb
+    % 0 - don't
+    perturb_row = 0;
+    perturb_col = 0;
+    
+    %  1 - do one only
+    %  0 - don't
+    % -1 - randomly select big changes
+    big_change_row = 0;
+    big_change_col = 0;
+    
+    % how it chooses which interaction to play with
+    %  1 - purely random choice 
+    %  0 - 50/50 from absent/existent things, then random from each of those
+    % -1 - only existing
+    % -2 - only non-existing
+    big_change_selection_method = -1
+    
+    % 1 - regen
+    % 0 - delete
+    big_change_delete_regen = 0;
+    
+    %% Interals
+    gene
+  end
+  
+  methods
+    %% Helper functions
+    
+    % Creates the mutator, sets the settings based on the input struct.
+    function self = Mutator(settings)
+      % Perturbations
+      switch settings.perturb
+        case 'both'
+          self.perturb_row = 1;
+          self.perturb_col = 1;
+        case 'row'
+          self.perturb_row = 1;
+        case 'col'
+          self.perturb_col = 1;
+      end
+      self.perturb_severity = settings.perturb_severity;
+      switch settings.perturb_style
+        case 'absolute'
+          self.perturb_style = 1;
+        case 'relative'
+          self.perturb_style = 0;
+      end
+      
+      % Random big change
+      switch settings.big_change
+        case 'both'
+          self.big_change_row = 1;
+          self.big_change_col = 1;
+        case 'row'
+          self.big_change_row = 1;
+        case 'col'
+          self.big_change_col = 1;
+      end
+      self.big_change_selection_method = settings.big_change_selection;
+      switch settings.delete_or_regen
+        case 'regen'
+          self.big_change_delete_regen = 1;
+        case 'delete'
+          self.big_change_delete_regen = 0;
+      end
+    end
+    
+    % Mutates the organism - calls the small mutation methods individually
+    % based on the settings that were given at creation
+    function org = mutate(self, org)
+      % useful
+      self.gene = org.duplicated;
+      
+      % Repetitive code, but does the job fine.
+      if self.perturb_row == 1
+        org.k = self.do_perturb_row(org);
+      end
+      
+      if self.perturb_col == 1
+        org.k = self.do_perturb_col(org);
+      end
+      
+      if self.big_change_row == 1
+        org.k = self.do_big_change(org, 'row');
+      end
+      
+      if self.big_change_col == 1
+        org.k = self.do_big_change(org, 'col');
+      end
+    end
+    
+    %% Actual mutation functions 
+    function k = do_perturb_row(self, org)
+      k = org.k;
+      
+      % Choose the "base" - absolute or relative perturbation
+      if self.perturb_style == 1
+        base = k(self.gene, :) ~= 0;
+      else
+        base = k(self.gene, :);
+      end
+      
+      % apply it
+      k(self.gene, :) = k(self.gene, :) + ...
+        base * self.perturb_severity .* (2*rand(1,org.size) - 1);
+    end
+    
+    function k = do_perturb_col(self, org)
+      k = org.k;
+      
+      % Choose the "base" - absolute or relative perturbation
+      if self.perturb_style == 1
+        base = k(:, self.gene) ~= 0;
+      else
+        base = k(:, self.gene);
+      end
+      
+      % apply it
+      k(:, self.gene) = k(:, self.gene) + ...
+        base * self.perturb_severity .* (2*rand(org.size,1) - 1);
+    end
+    
+    % DRY - transpose the matrix and apply a random row mutation if a
+    % column mutation is what was required.
+    function k = do_big_change(self, org, orientation)
+      k = org.k;
+      if orientation == 'col'
+        k = k';
+      end
+      
+      % Create a list of possibly mutatable indices of the row
+      switch self.big_change_selection_method
+        case 1
+          possible = find(k(self.gene, :) < 100);
+        case 0
+          % If there exists 1 or 0 possible interactions then DO THE OTHER THING
+          % Note: while loop is a pretty lazy way of doing this, but is easiest,
+          % and doesn't cause any massive inefficiencies.
+          possible = [];
+          while length(possible) < 2
+            if rand() < 0.5
+              possible = find(k(self.gene, :) ~= 0);
+            else
+              possible = find(k(self.gene, :) == 0);
+            end
+          end
+        case -1
+          possible = find(k(self.gene, :) ~= 0);
+        case -2
+          possible = find(k(self.gene, :) == 0);
+      end
+      
+      % Pick something random from the possible options
+      chosen = randi(length(possible), 1);
+      
+      % The class of teh /acting/ protein is based upon whether we are
+      % acting on rows or close
+      if orientation == 'col'
+        % This is horrible and hacky, the -1 is needed because this vector
+        % isn't updated upon duplication - but this is the only reason it
+        % woudl be required, so it's done here. TODO: refactor if
+        % necessary.
+        if chosen > self.gene
+          class = org.protein_categorisations(chosen-1);
+        else
+          class = org.protein_categorisations(chosen);
+        end
+      else
+        class = org.protein_categorisations(self.gene);
+      end
+      
+      % If it exists, regen or delete
+      if k(self.gene, chosen) ~= 0
+        % Regen
+        if self.big_change_delete_regen == 1
+          switch class
+            case 1
+              k(self.gene, chosen) = rand();
+            case 2
+              k(self.gene, chosen) = rand();
+            case 3
+              k(self.gene, chosen) = - rand();
+            case 4
+              k(self.gene, chosen) = 2*rand()-1;
+          end
+        % Delete
+        else
+          k(self.gene, chosen) = 0;
+        end
+      % If it doesn't, create
+      else
+          switch class
+            case 1
+              k(self.gene, chosen) = rand();
+            case 2
+              k(self.gene, chosen) = rand();
+            case 3
+              k(self.gene, chosen) = - rand();
+            case 4
+              k(self.gene, chosen) = 2*rand()-1;
+          end
+      end % End of if ==0 or not thing
+      
+      % Transpose it again if required
+      if orientation == 'col'
+        k = k';
+      end
+    end
+  end
+  
+end
+
---
+++ proccessnetworks.m
@@ -0,0 +1,168 @@
+% Script to handle the processing (duplication and mutation analysis)
+% of existing networks. Designed to run in parallel with other processors.
+%
+% Expects Data/Networks/queue to have be set up by ./preprocessing
+%   - to_process
+%   - processing
+%   - processed
+%
+% Outline: finds a network to process from the list, and perform some file
+% manipulation to keep this list in check. THEN, does analysis on this network.
+% THEN, does more file manipulation on the lists in order to tidy them up.
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% %
+%  Initially: Set simulation and mutation settings here
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% %
+
+% Settings
+settings_mutation = struct(...
+  'number', 20, ...
+  'perturb_style', 'relative', ...
+  'perturb', 'row', ...
+  'perturb_severity', 0.1, ...
+  'big_change', 'row', ...
+  'big_change_selection', 0, ...
+  'delete_or_regen', 'delete');
+settings_simulation = struct();
+
+% Seed random number generator
+[dummy_variable, host] = system('hostname');
+hex = DataHash(strcat(host,datestr(clock)));
+seed = mod(hex2dec(hex(1:16)), 2^32);
+s = RandStream('mt19937ar','Seed', seed);
+RandStream.setDefaultStream(s)
+
+% Directory layout
+from_root = 'Data/Networks/';
+to_root = 'Data/Dynamics/';
+to_process = strcat(from_root, 'to_process.txt');
+processing = strcat(from_root, 'processing');
+processed = strcat(from_root, 'processed');
+
+% Create mutator object
+mut = Mutator(settings_mutation);
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% %
+%  Processing control
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% %
+
+% Hacky method of do { } while (); in matlab
+while true
+  % Load list of organisms
+  list = textread(to_process, '%s');
+  % If there exists nothing that needs processing
+  if length(list) == 0
+    break
+  else
+    % Pick random element from list
+    el = randi(length(list));
+    ele = cell2mat(list(el));
+
+    % Remove the element from to_process
+    % Note: probably not efficient, but is not relatively slow
+    a = []; b = [];
+    if el ~= 1
+      a = list(1:(el-1));
+    end
+    if el ~= length(list)
+      b = list((el+1):length(list));
+    end
+    list_new = [a; b];
+    fid = fopen(to_process, 'w');
+    for i=1:length(list_new)
+      fprintf(fid, '%s\n', cell2mat(list_new(i)));
+    end
+    fclose(fid);
+
+    % Add element to processing queue
+    fid = fopen(processing, 'a');
+    fprintf(fid, '%s\n', ele);
+    fclose(fid);
+  end
+
+  % Load the organism
+  data_file = strcat(from_root, ele, '.mat');
+  data = load(data_file);
+  wt = data.organism;
+
+  % Useful
+  n = wt.size;
+  m = settings_mutation.number;
+
+  % Set up variables useful for processing
+  D_wt = data.D;
+  D_dup = zeros(1, n-1);
+  class = zeros(1,n-1);
+  D_dup_m = zeros(m,n-1);
+  D_wt_m = zeros(m,n-1);
+  mut_eff = zeros(m,n-1);
+  
+  % Loop through all genes
+  for i=1:(n-1)
+    % Create duplicate, and see what the dynamics of the duplicate is
+    dup = wt.duplicategene(i);
+    sim_dup = Simulation(dup);
+
+    % Dynamics = ?
+    if sim_dup.completed == 1
+      D_dup(i) = sim_dup.dynamics;
+    else
+      D_dup(i) = Inf;
+    end
+
+    % Class of protein = ?
+    class(i) = wt.protein_categorisations(i);
+
+    % Now perform mutations -- we do mutations even if duplication creates an
+    % unstable system -- mutation may make stable again
+    for j=1:m
+      % Create a mutated duplicate
+      dup_m = mut.mutate(dup);
+      sim_dup_m = Simulation(dup_m);
+
+      % Dynamics = ?
+      if sim_dup_m.completed == 1
+        D_dup_m(j,i) = sim_dup_m.dynamics;
+      else
+        D_dup_m(j,i) = Inf;
+      end
+
+      % Create a mutated WT
+      wt_m = wt;
+      a = dup_m.k(i,:);
+      b = [ a(1:(i-1)) , a((i+1):length(a)) ];
+      wt_m.k(i,:) = b;
+      sim_wt_m = Simulation(wt_m);
+
+      % Dynamics = ?
+      if sim_wt_m.completed == 1
+        D_wt_m(j,i) = sim_wt_m.dynamics;
+      else
+        D_wt_m(j,i) = Inf;
+      end
+
+      % Effects of mutation = ?
+      eff = sum(sum(dup_m.k - dup.k));
+      mut_eff(j,i) = eff;
+    end
+  end
+
+  % Save data
+  save(strcat(to_root,ele,'.mat'), ...
+    'D_wt', 'D_dup', 'class', 'D_dup_m', 'D_wt_m', 'mut_eff');
+
+  % add to processed
+  fid = fopen(processed, 'a');
+  fprintf(fid, '%s\n', ele);
+  fclose(fid);
+
+  % remove from processing queue
+  p = textread(processing, '%s');
+  fid = fopen(processing, 'w');
+  for i=1:length(p)
+    if ~strcmp(cell2mat(p(i)) , ele)
+      fprintf(fid, '%s\n', cell2mat(p(i)));
+    end
+  end
+  fclose(fid);
+end
